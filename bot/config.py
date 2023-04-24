fdaccbe36fc3002ac29ec9bf9cc4240ad0307c41
gjayanna2 <123467359+gjayanna2@users.noreply.github.com>
2023-04-24 20:08:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,15 +3,15 @@ import os
 
 class Config:
 
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID","3393749"))
+    API_HASH = os.environ.get("API_HASH","a15a5954a1db54952eebd08ea6c68b71")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN","1834175795:AAHMyQudBP_f4JVxbIwXSMD9u2OJ5hF8FLA")
     SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
-    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
-    MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL","-1001939303024"))
+    DATABASE_URL = os.environ.get("DATABASE_URL","mongodb+srv://Jayanna:Jayanna2023@yash.tm1c2bd.mongodb.net/?retryWrites=true&w=majority")
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1061576483").split(" ")]
+    MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 20))
+    MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 10000))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
     SLOW_SPEED_DELAY = int(os.environ.get("SLOW_SPEED_DELAY", 5))
     HOST = os.environ.get("HOST", "")
